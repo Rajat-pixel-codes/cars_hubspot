bf02f06526770bd397731c154f536cd52b18c971
Rajat Gupta <rajatsmart2001@gmail.com>
2023-08-17 18:06:46 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+/** @format */
+
+import {Footer, Navbar} from "@/components";
+import "./globals.css";
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: "Car Hub",
+  description: "Discover the best selected cars by experts",
+};
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  images: {
+    domains: ["cdn.imagin.studio"],
+  },
+};
+
+module.exports = nextConfig;
